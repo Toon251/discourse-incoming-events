b2a576e4c1940b717bfa74a73891f08e8710a0d1
Toon251 <toon251@gmail.com>
2025-06-06 05:02:01 +0000

    Change to decorate widget

--- javascripts/discourse/initializers/incoming-event.js
+++ javascripts/discourse/initializers/incoming-event.js
@@ -2,5 +2,6 @@ import { apiInitializer } from 'discourse/lib/api';
 import IncomingEvent from '../components/incoming-event';
 
 export default apiInitializer('1.14.0', (api) => {
-    api.renderInOutlet(settings.plugin_outlet.trim(), IncomingEvent);
+    //api.renderInOutlet(settings.plugin_outlet.trim(), IncomingEvent);
+    api.decorateWidget(settings.plugin_outlet.trim(), () => IncomingEvent);
 });
