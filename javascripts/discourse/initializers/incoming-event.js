15d9ae9b59a751d4d5e42885d2d8825295905cf3
Toon251 <toon251@gmail.com>
2025-06-13 02:42:14 +0000

    Debug isMobile

--- javascripts/discourse/initializers/incoming-event.js
+++ javascripts/discourse/initializers/incoming-event.js
@@ -8,12 +8,14 @@ export default apiInitializer('1.14.0', (api) => {
     }
 
     
+    const isMobile = window.matchMedia("(max-width: 767px)").matches;
+    console.log(isMobile);
 
     if(isMobile){
-        console.log("Renden in ", settings.plugin_outlet_mobile.trim())
+        console.log("Renden in mobil ", settings.plugin_outlet_mobile.trim())
         api.renderInOutlet(settings.plugin_outlet_mobile.trim(), IncomingEvent);
     }else{
-        console.log("Renden in ", settings.plugin_outlet.trim())
+        console.log("Renden in desktop ", settings.plugin_outlet.trim())
         api.renderInOutlet(settings.plugin_outlet.trim(), IncomingEvent);
     }
 
